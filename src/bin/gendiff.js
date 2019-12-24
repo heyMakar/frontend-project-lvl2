f10f6e65dfa394a93edc139d65dd6c8b59afb135
heymakar <heykaash@gmail.com>
2019-12-24 15:55:11 +0600

    commander setup

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,12 @@
-console.log('hi!');
+#!/usr/bin/env node
+
+import commander from 'commander';
+
+const program = commander;
+
+program
+  .version('1.0.0')
+  .description('simple description')
+  .parse(process.argv);
+
+if (!program.args.length) program.help();
