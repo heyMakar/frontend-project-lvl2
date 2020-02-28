bd60ab266d14a7f71e7a1952fdaacafde351601d
heymakar <heykaash@gmail.com>
2020-02-28 15:29:36 +0600

    add tree format render & update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,31 @@
 import fs from 'fs';
-import genDiff from '../src';
+import buildAst from '../src/ast';
+import treeFormatRender from '../src/renderers/treeFormat';
 
-const beforeJson = '__fixtures__/before.json';
-const afterJson = '__fixtures__/after.json';
-const beforeYml = '__fixtures__/before.yml';
-const afterYml = '__fixtures__/after.yml';
-const beforeIni = '__fixtures__/before.ini';
-const afterIni = '__fixtures__/after.ini';
+const flatBeforeJson = '__tests__/__fixtures__/flatBefore.json';
+const flatAfterJson = '__tests__/__fixtures__/flatAfter.json';
+const treeBeforeJson = '__tests__/__fixtures__/treeBefore.json';
+const treeAfterJson = '__tests__/__fixtures__/treeAfter.json';
+const treeBeforeYml = '__tests__/__fixtures__/treeBefore.yml';
+const treeAfterYml = '__tests__/__fixtures__/treeAfter.yml';
+const treeBeforeIni = '__tests__/__fixtures__/treeBefore.ini';
+const treeAfterIni = '__tests__/__fixtures__/treeAfter.ini';
 
-const pathToJsonResult = '__fixtures__/json.txt';
-const readJsonResult = fs.readFileSync(pathToJsonResult, 'utf-8');
+const pathToFlatResult = '__tests__/__fixtures__/flatJson.txt';
+const pathToTreeResult = '__tests__/__fixtures__/tree.txt';
+const flatResult = fs.readFileSync(pathToFlatResult, 'utf-8');
+const treeResult = fs.readFileSync(pathToTreeResult, 'utf-8');
 
-const pathToYmlResult = '__fixtures__/yml.txt';
-const readYmlResult = fs.readFileSync(pathToYmlResult, 'utf-8');
+test('make flat format diff', () => {
+  const ast = buildAst(flatBeforeJson, flatAfterJson);
+  expect(treeFormatRender(ast)).toEqual(flatResult);
+});
 
-const pathToIniResult = '__fixtures__/ini.txt';
-const readIniResult = fs.readFileSync(pathToIniResult, 'utf-8');
-
-test('make string', () => {
-  expect(genDiff(beforeJson, afterJson)).toEqual(readJsonResult);
-  expect(genDiff(beforeYml, afterYml)).toEqual(readYmlResult);
-  expect(genDiff(beforeIni, afterIni)).toEqual(readIniResult);
+test('make tree format diff', () => {
+  const astJson = buildAst(treeBeforeJson, treeAfterJson);
+  const astYml = buildAst(treeBeforeYml, treeAfterYml);
+  const astIni = buildAst(treeBeforeIni, treeAfterIni);
+  expect(treeFormatRender(astJson)).toEqual(treeResult);
+  expect(treeFormatRender(astYml)).toEqual(treeResult);
+  expect(treeFormatRender(astIni)).toEqual(treeResult);
 });
---
+++ src/renderers/treeFormat.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-multi-spaces */
+
+import { keys, isObject, flatten } from 'lodash';
+
+const indentPlacer = (repeats) => {
+  const  tab = '  ';
+  return tab.repeat(repeats);
+};
+
+const stringify = (obj, depth) => {
+  if (isObject(obj)) {
+    const key = keys(obj)[0];
+    const currentDepth = depth === 0 ? 2 : depth;
+    const breakLine = currentDepth === 0 ? '' : '\n';
+    return `{${breakLine}${indentPlacer(currentDepth)}    ${key}: ${obj[key]}${breakLine}${indentPlacer(currentDepth)}}`;
+  }
+  return obj;
+};
+
+const renderObject = (obj, depth = 0) => {
+  const {
+    key, status, value, children,
+  } = obj;
+  const childs = flatten(children);
+  const breakLine = depth === 0 ? '' : '\n';
+  switch (status) {
+    case 'added':
+      return `${breakLine}${indentPlacer(depth)}  + ${key}: ${stringify(value[key], depth * 2)}`;
+    case 'removed':
+      return `${breakLine}${indentPlacer(depth)}  - ${key}: ${stringify(value[key], depth * 2)}`;
+    case 'changed': {
+      const { valueBefore, valueAfter } = value;
+      const before = valueBefore[key];
+      const after = valueAfter[key];
+      const beforeToString = isObject(before) ? stringify(before, depth * 2) : before;
+      const afterToString = isObject(after) ? stringify(after, depth * 2) : after;
+      return `${breakLine}${indentPlacer(depth)}  - ${key}: ${beforeToString}\n${indentPlacer(depth)}  + ${key}: ${afterToString}`;
+    }
+    case 'unchanged':
+      return `${breakLine}${indentPlacer(depth)}    ${key}: ${value[key]}`;
+    case 'nested':
+      return `${breakLine}${indentPlacer(depth)}    ${key}: {${childs.map((c) => renderObject(c, depth * 2)).join('')}${breakLine}${indentPlacer(depth)}    }`;
+    default:
+      return 'wrong status';
+  }
+};
+
+const render = (tree) => {
+  const reducedTree = tree.reduce((acc, item) => {
+    const childs = flatten(item.children);
+    const depth = 1;
+    const depthStep = 1;
+    if (childs.length > 0) {
+      const nestedDepth = depth + depthStep;
+      const renderChilds = childs.map((c) => renderObject(c, nestedDepth));
+      return [...acc, `${indentPlacer(depth)}  ${item.key}: {${renderChilds.join('')}\n${indentPlacer(depth)}  }`];
+    }
+    return [...acc, renderObject(item)];
+  }, []);
+  return `{\n${reducedTree.join('\n')}\n}`;
+};
+
+export default render;
