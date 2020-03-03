5bf5cb2a24089c277f2ab90e0e905f3ed568c8a4
heymakar <heykaash@gmail.com>
2020-03-03 13:11:11 +0600

    flag setup/description

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,10 +6,12 @@ import genDiff from '..';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', `output format: default tree;
+  'tree' set output to show a tree difference (unchanges include),
+  'plain' set output to show a plain difference (unchanges exclude)`)
   .arguments('<firstConfig> <secondConfig>')
   .action((path1, path2) => {
-    console.log(genDiff(path1, path2));
+    console.log(genDiff(path1, path2, program.format));
   });
 
 program.parse(process.argv);
