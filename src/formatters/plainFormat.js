755dbe83d3a239072bfe86b42b75b1f14e7e1d70
heymakar <heykaash@gmail.com>
2020-03-03 14:29:53 +0600

    names fix

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -1,24 +1,26 @@
 import { trim, isObject, flatten } from 'lodash';
 
-const complexValue = (value) => (isObject(value) ? '[complex value]' : `${value}`);
+const complexValue = (value) => (isObject(value) ? '[complex value]' : value);
 
 const renderPlain = (tree, path = '') => tree
   .filter((item) => item.status !== 'unchanged')
-  .map((item) => {
-    const nestedPath = `${path}.${item.key}`;
-    switch (item.status) {
+  .map(({
+    key, value, status, children,
+  }) => {
+    const nestedPath = `${path}.${key}`;
+    switch (status) {
       case 'added':
-        return `Property ${trim(nestedPath, '.')} was added with value: ${complexValue(item.value[item.key])}`;
+        return `Property ${trim(nestedPath, '.')} was added with value: ${complexValue(value[key])}`;
       case 'removed':
         return `Property ${trim(nestedPath, '.')} was removed`;
       case 'changed': {
-        const { valueBefore, valueAfter } = item.value;
-        const before = complexValue(valueBefore[item.key]);
-        const after = complexValue(valueAfter[item.key]);
+        const { valueBefore, valueAfter } = value;
+        const before = complexValue(valueBefore[key]);
+        const after = complexValue(valueAfter[key]);
         return `Property ${trim(nestedPath, '.')} was changed from '${before}' to '${after}'`;
       }
       case 'nested':
-        return renderPlain(flatten(item.children), nestedPath);
+        return renderPlain(flatten(children), nestedPath);
       default:
         return 'wrong status';
     }
