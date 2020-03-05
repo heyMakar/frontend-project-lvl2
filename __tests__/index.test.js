1a0b53304e2b2773439c1721ca0566a996abf413
heymakar <heykaash@gmail.com>
2020-03-05 12:14:22 +0600

    rework logic in tests/add new fixture

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,41 +1,37 @@
 import fs from 'fs';
-import genDiff from '../src/index';
+import path from 'path';
+import genDiff from '../src';
 
-const flatBeforeJson = `${__dirname}/__fixtures__/flatBefore.json`;
-const flatAfterJson = `${__dirname}/__fixtures__/flatAfter.json`;
-const treeBeforeJson = `${__dirname}/__fixtures__/treeBefore.json`;
-const treeAfterJson = `${__dirname}/__fixtures__/treeAfter.json`;
-const treeBeforeYml = `${__dirname}/__fixtures__/treeBefore.yml`;
-const treeAfterYml = `${__dirname}/__fixtures__/treeAfter.yml`;
-const treeBeforeIni = `${__dirname}/__fixtures__/treeBefore.ini`;
-const treeAfterIni = `${__dirname}/__fixtures__/treeAfter.ini`;
+const methods = [
+  'tree',
+  'plain',
+  'json',
+];
 
-const pathToFlatResult = `${__dirname}/__fixtures__/flatJson.txt`;
-const pathToTreeResult = `${__dirname}/__fixtures__/tree.txt`;
-const pathToPlainResult = `${__dirname}/__fixtures__/plain.txt`;
-const pathToJsonResult = `${__dirname}/__fixtures__/json.txt`;
+const extensions = [
+  ['.ini'],
+  ['.json'],
+  ['.yml'],
+];
 
-const flatResult = fs.readFileSync(pathToFlatResult, 'utf-8');
-const treeResult = fs.readFileSync(pathToTreeResult, 'utf-8');
-const plainResult = fs.readFileSync(pathToPlainResult, 'utf-8');
-const jsonResult = fs.readFileSync(pathToJsonResult, 'utf-8');
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('make flat format diff', () => {
-  expect(genDiff(flatBeforeJson, flatAfterJson)).toEqual(flatResult);
+test('make flat diff', () => {
+  const flatBefore = getFixturePath('flatBefore.json');
+  const flatAfter = getFixturePath('flatAfter.json');
+  const flatResult = readFile('flatJson.txt');
+  expect(genDiff(flatBefore, flatAfter)).toEqual(flatResult);
 });
 
-test('make tree format diff', () => {
-  expect(genDiff(treeBeforeJson, treeAfterJson)).toEqual(treeResult);
-  expect(genDiff(treeBeforeYml, treeAfterYml)).toEqual(treeResult);
-  expect(genDiff(treeBeforeIni, treeAfterIni)).toEqual(treeResult);
-});
-
-test('make plain format diff', () => {
-  expect(genDiff(treeBeforeJson, treeAfterJson, 'plain')).toEqual(plainResult);
-  expect(genDiff(treeBeforeYml, treeAfterYml, 'plain')).toEqual(plainResult);
-  expect(genDiff(treeBeforeIni, treeAfterIni, 'plain')).toEqual(plainResult);
-});
-
-test('make json format diff', () => {
-  expect(genDiff(treeBeforeJson, treeAfterJson, 'json')).toEqual(jsonResult);
-});
+methods.forEach((method) => test.each(extensions)(`make ${method} diff`, (extension) => {
+  const before = getFixturePath(`treeBefore${extension}`);
+  const after = getFixturePath(`treeAfter${extension}`);
+  /*
+  ini parser parse numbers with quotes in some cases(bug),
+  so we pick another fixture for compare
+  */
+  const expected = (method === 'json' && extension === '.ini') ? readFile('jsonIni.txt') : readFile(`${method}.txt`);
+  const recieved = genDiff(before, after, method);
+  expect(recieved).toEqual(expected);
+}));
