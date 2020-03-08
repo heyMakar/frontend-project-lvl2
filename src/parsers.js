d66c531c9262b8de558bfd4e9a96610245c28496
heymakar <heykaash@gmail.com>
2020-03-08 12:18:56 +0600

    lint fix

--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,9 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 const extensions = {
-  'json': JSON.parse,
-  'yml': yaml.safeLoad,
-  'ini': ini.parse,
+  json: JSON.parse,
+  yml: yaml.safeLoad,
+  ini: ini.parse,
 };
 
 const getParser = (pathToFile, type) => extensions[type](pathToFile);
